294fcea21216f9448588dd004ecf2671c129b3a1
jyang-mscs <jeffersonyang01@gmail.com>
2025-08-28 08:48:09 -0500

    Initial Commit

---
+++ xc16-source-control/xc16_source_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  xc16_source_controlApp.swift
+//  xc16-source-control
+//
+//  Created by Jeff Yang on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct xc16_source_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
